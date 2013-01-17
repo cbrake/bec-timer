bc62b9f91ecc0b433dec60af22d426aac1c3f739
Cliff Brake <cbrake@bec-systems.com>
2013-01-17 11:40:58 -0500

    add angular and button to play audio

---
+++ client/code/libs/audio.js
@@ -0,0 +1,16 @@
+function AudioCtrl($scope) {
+  console.log('AudioCtrl')
+
+  var audio = new Audio();
+
+  $scope.playAudio = function() {
+    console.log("Play Audio")
+    // mp3 does not work in firefox
+    //audio.type = "audio/mpeg"
+    //audio.src = "/beepbeep.mp3"
+    // ogg works in both firefox and chrome
+    audio.type = "audio/ogg"
+    audio.src = "/beepbeep.ogg"
+    audio.play()
+  };
+}
