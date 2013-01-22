21d2f9507195246fe2bf383f9f3ffb8fc621309a
Cliff Brake <cbrake@bec-systems.com>
2013-01-22 15:41:22 -0500

    add client code that ding's on 15m intervals

--- client/code/libs/audio.js
+++ client/code/libs/audio.js
@@ -2,15 +2,27 @@ function AudioCtrl($scope) {
   console.log('AudioCtrl')
 
   var audio = new Audio();
+  audio.type = "audio/mpeg"
+  audio.src = "/DingDong.mp3"
+
+  $scope.schedule_next_ding = function() {
+    var d = new Date();
+    var min = d.getMinutes()
+    console.log("min = " + min)
+    var next_15m_time = Math.floor((min + 15)/15) * 15
+    console.log("next_15m_time = " + next_15m_time)
+    setTimeout($scope.timeout, (next_15m_time - min)*60*1000)
+  }
+
+  $scope.timeout = function() {
+    audio.play()
+    $scope.schedule_next_ding();
+  }
 
   $scope.playAudio = function() {
     console.log("Play Audio")
-    // mp3 does not work in firefox
-    audio.type = "audio/mpeg"
-    audio.src = "/DingDong.mp3"
-    // ogg works in both firefox and chrome
-    //audio.type = "audio/ogg"
-    //audio.src = "/beepbeep.ogg"
     audio.play()
   };
+
+  $scope.schedule_next_ding()
 }
